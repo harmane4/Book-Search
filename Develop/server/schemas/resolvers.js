4440ebc0cbc1d8952259189c6519604b9e71eb1b
Elise Harman <elise_harman@hotmail.com>
2021-08-02 13:40:22 +0800

    Updated server.js + auth.js. Added schemas directory - resolvers.js needs to be completed

---
+++ Develop/server/schemas/resolvers.js
@@ -0,0 +1,11 @@
+const User = require("../models");
+
+// TODO:Define the query and mutation functionality to work with the Mongoose models.
+
+const resolvers = {
+  Query: {},
+
+  Mutation: {},
+};
+
+module.exports = resolvers;
