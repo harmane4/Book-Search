0bb2747b5d6cb949857da660e5dbb8e94ffddf68
Elise Harman <elise_harman@hotmail.com>
2021-08-03 12:55:49 +0800

    Fixed ADD_USER mutation error

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -13,10 +13,13 @@ export const LOGIN_USER = gql`
 `;
 
 export const ADD_USER = gql`
-  mutation addUser($username: String, $email: String, $password: String) {
+  mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
       token
-      user
+      user {
+        _id
+        username
+      }
     }
   }
 `;
